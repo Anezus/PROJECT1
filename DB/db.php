6e682f96a09a1f4a80daf8588ad9ad3917cf3267
HuGoTran-dev <tranngochoanganh06@gmail.com>
2025-04-16 11:40:19 +0700

    da fix

--- DB/db.php
+++ DB/db.php
@@ -1,7 +1,7 @@
 <?php
 $server = "Localhost";
 $port = 3306;
-$database = "shoppingcartss8";
+$database = "project1";
 $username = "root";
 $password = "";
 $conn = new mysqli($server, $username, $password, $database, $port);
